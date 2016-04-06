943d696feefdf2da20d97b656a108b97ff032253
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2016-04-06 11:16:03 +0100

    API-1213 - API revocation frontend

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # api-revocation-frontend
 
 [![Build Status](https://travis-ci.org/hmrc/api-revocation-frontend.svg?branch=master)](https://travis-ci.org/hmrc/api-revocation-frontend) [ ![Download](https://api.bintray.com/packages/hmrc/releases/api-revocation-frontend/images/download.svg) ](https://bintray.com/hmrc/releases/api-revocation-frontend/_latestVersion)
@@ -7,5 +6,4 @@ This is a placeholder README.md for a new repository
 
 ### License
 
-This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
-    
+This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html")
---
+++ project/FrontendBuild.scala
@@ -0,0 +1,63 @@
+import sbt._
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object FrontendBuild extends Build with MicroService {
+
+  val appName = "api-revocation-frontend"
+
+  override lazy val plugins: Seq[Plugins] = Seq(
+    SbtAutoBuildPlugin, SbtGitVersioning, SbtDistributablesPlugin
+  )
+
+  override lazy val appDependencies: Seq[ModuleID] = AppDependencies()
+}
+
+private object AppDependencies {
+  import play.PlayImport._
+  import play.core.PlayVersion
+
+  private val playHealthVersion = "1.1.0"    
+  private val playJsonLoggerVersion = "2.1.1"      
+  private val frontendBootstrapVersion = "6.3.0"
+  private val govukTemplateVersion = "4.0.0"
+  private val playUiVersion = "4.10.0"
+  private val playPartialsVersion = "4.2.0"
+  private val playAuthorisedFrontendVersion = "4.7.0"
+  private val playConfigVersion = "2.0.1"
+  private val hmrcTestVersion = "1.6.0"
+  
+  val compile = Seq(
+    ws,
+    "uk.gov.hmrc" %% "frontend-bootstrap" % frontendBootstrapVersion,
+    "uk.gov.hmrc" %% "play-partials" % playPartialsVersion,
+    "uk.gov.hmrc" %% "play-authorised-frontend" % playAuthorisedFrontendVersion,
+    "uk.gov.hmrc" %% "play-config" % playConfigVersion,
+    "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
+    "uk.gov.hmrc" %% "govuk-template" % govukTemplateVersion,
+    "uk.gov.hmrc" %% "play-health" % playHealthVersion,
+    "uk.gov.hmrc" %% "play-ui" % playUiVersion
+  )
+
+  trait TestDependencies {
+    lazy val scope: String = "test"
+    lazy val test : Seq[ModuleID] = ???
+  }
+
+  object Test {
+    def apply() = new TestDependencies {
+      override lazy val test = Seq(
+        "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
+        "org.scalatest" %% "scalatest" % "2.2.6" % scope,
+        "org.pegdown" % "pegdown" % "1.5.0" % scope,
+        "org.jsoup" % "jsoup" % "1.7.3" % scope,
+        "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+      )
+    }.test
+  }
+
+  def apply() = compile ++ Test()
+}
+
+
