bbbbc34c17c3afa3c3f3a485e8ce6cb297539d4e
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2024-02-13 15:20:45 +0000

    APIS-6805 - Pr comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,21 +3,20 @@ import sbt._
 
 object AppDependencies {
 
-  lazy val bootstrapVersion = "8.4.0"
-  lazy val commonDomainVersion      = "0.10.0"
+  lazy val bootstrapVersion    = "8.4.0"
+  lazy val commonDomainVersion = "0.11.0"
 
   def apply(): Seq[ModuleID] = compile ++ test
 
   lazy val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"         % "8.4.0"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "8.4.0"
   )
 
   lazy val test = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-test-play-30"   % bootstrapVersion,
-    "org.jsoup"              % "jsoup"                    % "1.10.2",
-    "org.mockito"           %% "mockito-scala-scalatest"  % "1.17.29",
-    "uk.gov.hmrc"           %% "api-platform-test-common-domain" % commonDomainVersion
-
+    "uk.gov.hmrc" %% "bootstrap-test-play-30"          % bootstrapVersion,
+    "org.jsoup"    % "jsoup"                           % "1.10.2",
+    "org.mockito" %% "mockito-scala-scalatest"         % "1.17.29",
+    "uk.gov.hmrc" %% "api-platform-test-common-domain" % commonDomainVersion
   ).map(_ % "test")
 }
