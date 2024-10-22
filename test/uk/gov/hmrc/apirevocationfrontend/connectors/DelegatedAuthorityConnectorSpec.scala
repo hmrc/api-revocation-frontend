d81dae998db1058806f2f0bb5a768603f14b4fc9
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2024-10-22 14:53:31 +0100

    API-7925: bump common library version

--- test/uk/gov/hmrc/apirevocationfrontend/connectors/DelegatedAuthorityConnectorSpec.scala
+++ test/uk/gov/hmrc/apirevocationfrontend/connectors/DelegatedAuthorityConnectorSpec.scala
@@ -24,8 +24,8 @@ import com.codahale.metrics.SharedMetricRegistries
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 
 import uk.gov.hmrc.apiplatform.modules.common.utils.FixedClock
-import uk.gov.hmrc.http.client.HttpClientV2
 import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.client.HttpClientV2
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 import uk.gov.hmrc.apirevocationfrontend.models.{AppAuthorisation, Scope, ThirdPartyApplication}
